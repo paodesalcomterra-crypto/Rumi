24457f810595918d1941b27553e5e1ac526c5e7c
Roam <paodesalcomterra@gmail.com>
2026-06-17 18:14:28 -0300

    Corrige porta Render

--- backend/server.js
+++ backend/server.js
@@ -225,8 +225,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
   console.log(
-    "Servidor rodando na porta 3001"
+    `Servidor rodando na porta ${PORT}`
   );
 });

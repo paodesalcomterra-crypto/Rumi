29fbda9da4ba91e17e02cedfbcd197fa5e228dc6
Roam <paodesalcomterra@gmail.com>
2026-06-29 12:05:39 -0300

    de

--- frontend/src/components/GamesScreen/GamesScreen.jsx
+++ frontend/src/components/GamesScreen/GamesScreen.jsx
@@ -3,7 +3,7 @@ import Rumi2 from "../../assets/Rumi2.png";
 import TraveDireita from "../../assets/travedireita.png";
 import TraveEsquerda from "../../assets/traveesquerda.png";
 import Vs from "../../assets/vs.png";
-import PlayBottom from "../../assets/playbottom.png";
+import PlayerBottom from "../../assets/playerbottom.png";
 
 export default function GamesScreen({ onClose }) {
   return (
@@ -173,9 +173,9 @@ export default function GamesScreen({ onClose }) {
         <div className="games-play">
 
           <img
-            src={PlayBottom}
+            src={PlayerBottom}
             alt="Play"
-            className="play-bottom-image"
+            className="play-games-image"
           />
 
         </div>

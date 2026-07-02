3c10a554d79acfe052340460b5b2276732d8f34e
Roam <paodesalcomterra@gmail.com>
2026-07-02 13:35:18 -0300

    dfsfu

--- frontend/src/Room.jsx
+++ frontend/src/Room.jsx
@@ -2049,7 +2049,7 @@ overflowY: "auto",
                   src={msg.figurinha}
                   alt=""
                   style={{
-                    width: "110px",
+                    width: "90px",
                     maxWidth: "35vw",
                     borderRadius: "14px",
                     display: "block",

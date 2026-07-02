c6a994f1d2cefbed13e88600e1b8c79268f097ce
Roam <paodesalcomterra@gmail.com>
2026-07-02 13:39:09 -0300

    s

--- frontend/src/Room.jsx
+++ frontend/src/Room.jsx
@@ -2049,7 +2049,7 @@ overflowY: "auto",
                   src={msg.figurinha}
                   alt=""
                   style={{
-                    width: "98.9px",
+                    width: "101px",
                     maxWidth: "35vw",
                     borderRadius: "14px",
                     display: "block",

7b60a17daa5526fb70915718367fdeb7ed55fda6
Roam <paodesalcomterra@gmail.com>
2026-07-03 17:03:44 -0300

    dfs

--- frontend/src/Room.jsx
+++ frontend/src/Room.jsx
@@ -1280,7 +1280,7 @@ setOverlayIcon("play");
   left: "38px",     // aumenta a largura e vai mais para a esquerda
   right: "42px",    // continua sem encostar no botão de tela cheia
 
-  bottom: "58.4px",   // sobe um pouco
+  bottom: "49.4px",   // sobe um pouco
 
 
       zIndex: 9000,
